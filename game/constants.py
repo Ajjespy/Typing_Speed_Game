b5be7ebe8430d63fb7d93049c46ef9a430c06c50
EthanTolson <ethantolson1@gmail.com>
2022-05-18 14:01:33 -0600

    Cnahged width and height constants for scaling

--- game/constants.py
+++ game/constants.py
@@ -1,6 +1,6 @@
 from os import path
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = int(1920 /2)
+SCREEN_HEIGHT = int(1080 /2)
 SCREEN_TITLE = "Typing Game"
 RESOURCE_PATH = f"{path.dirname(path.abspath(__file__))}/resources/"
