d6b51f0516f8518e3e0b0efa5b3c59f6b7576dd8
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-06-13 13:39:31 -0600

    linted

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from arcade.color import ARSENIC
 from game.constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
 from game.mainmenu import MainMenu
 
+
 def main():
-    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable = False, fullscreen = True)
+    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=False, fullscreen=True)
     set_background_color(ARSENIC)
     game_view = MainMenu()
     game_view.setup()
     window.show_view(game_view)
     run()
+
 
 if __name__ == "__main__":
     main()
